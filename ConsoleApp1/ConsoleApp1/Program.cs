36cb6d3fbb82610857f11e015d84adfd38a998e9
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-22 14:32:57 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> workers = new Dictionary<string, string>();
+            workers.Add("Ivanov ivan ivanovich", "Ceo");
+            workers.Add("Petrov Petr Petrovich", "HR");
+            workers.Add("Vasiliev Vasiliy Vaslilevich", "Cteo");
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                Console.Clear();
+                Console.WriteLine("Список команд:");
+                Console.WriteLine("1.Добавить досье");
+                Console.WriteLine("2.Показать все досье");
+                Console.WriteLine("3.Удалить досье");
+                Console.WriteLine("4.Выход");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        AddDossier(workers);
+                        break;
+                    case "2":
+                        ShowList(workers);
+                        break;
+                    case "3":
+                        DeleteDossier(workers);
+                        break;
+                    case "4":
+                        Exit(ref isWorking);
+                        break;
+                    default :
+                        Console.WriteLine("Команда не распознана");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        static void AddDossier(Dictionary<string, string> workers)
+        {
+            Console.WriteLine("Ведите ФИО");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ведите ФИО");
+            string position = Console.ReadLine();
+            workers.Add(name, position);
+        }
+
+        static void ShowList(Dictionary<string, string> workers)
+        {
+            foreach (var worker in workers)
+            {
+                Console.WriteLine($"{worker.Key} - {worker.Value}");
+            }
+
+            Console.WriteLine("\nОжидается ввод для продолжения");
+            Console.ReadKey();
+        }
+
+        static void DeleteDossier(Dictionary<string, string> workers)
+        {
+            Console.WriteLine("Какое досье вы хотите удалить?");
+            string input = Console.ReadLine();
+            workers.Remove(input);
+        }
+        static void Exit(ref bool isWorking)
+        {
+            isWorking = false;
+        }
+    }
+}
